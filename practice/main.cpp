126c073b7a42a606f2ea54da94d3d0dbe8250386
Aaron Yang <ayang@ember-entertainment.com>
2016-07-29 10:20:40 -0700

    Initial Commit

---
+++ practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practice
+//
+//  Created by Aaron Yang on 7/29/16.
+//  Copyright © 2016 Aaron Yang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
